b3f14cd8029746e001f9e13c7b6d1db9a45b1c3c
memona92 <hammamiamneh7@gmail.com>
2020-05-03 09:45:03 +0300

    warpSolved

--- W5D2/W5D2.js
+++ W5D2/W5D2.js
@@ -34,9 +34,38 @@ not a global variable or only the specific example above.
 I.e. If someone called your function with 10 objects 
 (with all safely have the same properties), it should work just as well.
 */
+function each(coll,f){
+  if(Array.isArray(coll)){
+    for(var i=0; i<coll.length; i++){
+      f(coll[i],i);
+    }
+    for(var key in coll){
+      f(coll[key],key);
+    }
+  }
+}
 
-// your answer is here
+function filter(coll,predicate){
+var acc=[];
+if(!Array.isArray(coll)){
+  acc={};
+}
+each (coll,function(element,key){
+  if(predicate(element,key)){
+    acc[key]= element;
+  }
+})
+return acc;
+}
 
+
+function tallEnoughToRide(array){
+  return filter(array,function(element,key){
+    if(element.heightInInches >= 48){
+      return element.name 
+    }
+  });
+}
 /*
 2-Working off of the same data structure as tallEnoughToRide, 
 write a function called tallestPerson that takes  an array of people objects as well, 
@@ -51,3 +80,28 @@ tallestPerson(groupA); //"Kiana at 55 inches"
 */
 
 // your answer is here
+function reduce(arr,f,acc){
+  if(acc===undefined){
+    acc=arr[0];
+    arr=arr.slice(1);
+  }
+  each(arr,function (element,i){
+   acc=f(acc,element,i);
+  });
+  return acc;
+}
+
+
+function tallestPerson(array){
+  return reduce(array,function(element,key){
+     if(acc<element.heightInInches){
+     acc = element.heightInInches 
+   }
+   return acc.name + "at "+ acc + " inches"
+  });
+}
+
+
+
+
+
